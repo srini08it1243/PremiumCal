043a9e59e6082f5a16ab88b2bf5984b535dae28c
Your Name <srini.08it1243@gmail.com>
2025-11-18 21:57:43 +0530

    3rd commit

---
+++ Occupation_insertscript.sql
@@ -0,0 +1,6 @@
+use PremiumDB
+
+insert into Occupations(Name,RatingId) values('Doctor',1)
+insert into Occupations(Name,RatingId) values('Engineer',2)
+insert into Occupations(Name,RatingId) values('Mechanic',3)
+insert into Occupations(Name,RatingId) values('Builder',4)
---
+++ Rating_Inserscript.sql
@@ -0,0 +1,8 @@
+
+use PremiumDB
+
+ 
+  insert into Ratings(RatingName,Factor) values('Professional',1)
+  insert into Ratings(RatingName,Factor) values('White Collar',1.25)
+  insert into Ratings(RatingName,Factor) values('Light Manual',1.5)
+  insert into Ratings(RatingName,Factor) values('Heavy Manual',1.75)
